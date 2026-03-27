643fc2dbff8474ce4ee98878794fa95b1b947a58
CelidaDavila <celiximena1@gmail.com>
2026-03-27 08:33:39 -0600

    local changes

--- First-Come-Fisrt-Served/System.h
+++ First-Come-Fisrt-Served/System.h
@@ -17,6 +17,7 @@ using namespace std;
 using namespace std::this_thread;
 using namespace std::chrono;
 
+
 class System{
 private:
     queue<Process> newProcesses;
@@ -27,11 +28,63 @@ private:
     Process runningProcess;
     int globalClock;
     int allProcessNumber;
+    int id;
 public:
     System(){
         this->globalClock = 0;
         this->allProcessNumber = 0;
         this->hasRunningProcess = false;
+        this->id=0;
+        srand((unsigned)time(nullptr));
+    }
+
+    void registerProcesses(int numProcesses){
+        char operations[] = {'+', '-', '*', '/', '%', '^'};
+        char operation;
+        int cont = 0,estimatedTime,operand1,operand2;
+
+        while(cont < numProcesses){
+            id++;
+            operation = operations[rand() % 6];
+            operand1 = randInRange(-100, 100);
+            operand2 = randInRange(-100, 100);
+            operand2 = validateSecondOperand(operand2,operation);
+            estimatedTime = randInRange(6, 20);
+
+            createProcess(operation,operand1,operand2,estimatedTime,id);
+            cont++;
+        }
+    }
+
+    int randInRange(int lowBound, int highBound) {
+        return lowBound + (rand() % (highBound - lowBound + 1));
+    }
+
+    void createProcess(char operation,int operand1,int operand2,int estimatedTime,int id){
+
+        Process process(operation,operand1,operand2,estimatedTime,id);
+        addNewProcess(process);
+
+    }
+
+    int validateSecondOperand(int operand2,char operation){
+        if ((operation == '/' || operation == '%') && operand2 == 0){
+            operand2 = randInRange(-100, 100);
+        }
+        return operand2;
+    }
+
+
+    void startProgram(){
+        int numProcesses;
+        cout << "How many processes do you want to create?: ";
+        cin >> numProcesses;
+        cin.get();
+        registerProcesses(numProcesses);
+        run();
+        cout << "Press ENTER to list all process info...." << endl;
+        cin.get();
+        printAllInfo();
     }
     void addNewProcess(Process &process){
         newProcesses.push(process);
@@ -83,7 +136,7 @@ public:
 
     void printData(){
         clearScreen();
-        cout << "'I' = interrumpt 'E' = error 'P' = pause 'C'= continue " << endl;
+        cout << "'I' = interrumpt 'E' = error 'P' = pause 'C'= continue 'N' = new 'B' = processes table " << endl;
         cout << "GLOBAL TIME: " << globalClock << endl;
         cout << "Number of processes (New state): " << newProcesses.size() << endl;
         printReadyProcesses();
@@ -210,6 +263,21 @@ public:
                         }
                     }
                 break;
+            case 'N':
+                    {
+                       registerProcesses(1);
+                    }
+                break;
+            case 'B':
+                    {
+                        printInfoButtonB();
+                        cout << "Process paused... Press C to continue";
+                        char keyPressed = toupper(_getch());
+                            while(keyPressed != 'C'){
+                                keyPressed = toupper(_getch());
+                            }
+                    }
+                break;
             default:
                 break;
             }
@@ -250,7 +318,73 @@ public:
         printLongSeparator();
     }
 
+    void printInfoButtonB(){
+        clearScreen();
+        cout << "ALL PROCESSES INFO" << endl;
+        printLongSeparator();
+        cout << left << setw(8)  << "ID" << setw(10) << "Arrival" << setw(10) << "Finish"
+            << setw(14) << "Return" << setw(14) << "Response" << setw(10) << "Waiting"
+            << setw(10) << "Service" << setw(12) << "State" << endl;
+
+        printLongSeparator();
+        queue<Process> tempNew = newProcesses;
+        while(!tempNew.empty()){
+            Process process = tempNew.front();
+            tempNew.pop();
+            cout << left << setw(8) << process.getId()
+                 << setw(10) << process.getArrivalTime()
+                 << setw(10) << process.getFinishTime()
+                 << setw(14) << process.getReturnTime()
+                 << setw(14) << process.getResponseTime()
+                 << setw(10) << process.getWaitingTime()
+                 << setw(10) << process.getServiceTime()
+                 << setw(12) << process.getStatus() << endl;
+        }
+
+        queue<Process> tempReady = readyProcesses;
+        while(!tempReady.empty()){
+            Process process = tempReady.front();
+            tempReady.pop();
+            cout << left << setw(8) << process.getId()
+                 << setw(10) << process.getArrivalTime()
+                 << setw(10) << process.getFinishTime()
+                 << setw(14) << process.getReturnTime()
+                 << setw(14) << process.getResponseTime()
+                 << setw(10) << process.getWaitingTime()
+                 << setw(10) << process.getServiceTime()
+                 << setw(12) << process.getStatus() << endl;
+        }
+
+        if(hasRunningProcess){
+            cout << left << setw(8) << runningProcess.getId()
+                 << setw(10) << runningProcess.getArrivalTime()
+                 << setw(10) << runningProcess.getFinishTime()
+                 << setw(14) << runningProcess.getReturnTime()
+                 << setw(14) << runningProcess.getResponseTime()
+                 << setw(10) << runningProcess.getWaitingTime()
+                 << setw(10) << runningProcess.getServiceTime()
+                 << setw(12) << runningProcess.getStatus() << endl;
+        }
+
+        for (Process& process : blockedProcesses) {
+            cout << left << setw(8)  << process.getId() << setw(10) << process.getArrivalTime()
+                 << setw(10) << process.getFinishTime() << setw(14) << process.getReturnTime()
+                 << setw(14) << process.getResponseTime() << setw(10) << process.getWaitingTime()
+                 << setw(10) << process.getServiceTime() << setw(12) << process.getStatus() << endl;
+        }
+
+        for (Process& process : finishedProcesses) {
+            cout << left << setw(8)  << process.getId() << setw(10) << process.getArrivalTime()
+                 << setw(10) << process.getFinishTime() << setw(14) << process.getReturnTime()
+                 << setw(14) << process.getResponseTime() << setw(10) << process.getWaitingTime()
+                 << setw(10) << process.getServiceTime() << setw(12) << process.getStatus() << endl;
+        }
+        printLongSeparator();
+    }
+
 
 };
+
+
 
 #endif // SYSTEM_H_INCLUDED
--- First-Come-Fisrt-Served/main.cpp
+++ First-Come-Fisrt-Served/main.cpp
@@ -2,73 +2,16 @@
 //First Come First Served (FCFS)
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
 #include "System.h"
 
 using namespace std;
 
-System os;
-
-void registerProcesses(int numProcesses);
-int randInRange(int lowBound, int highBound);
-void createProcess(char operation,int operand1,int operand2,int estimatedTime,int id);
-int validateSecondOperand(int operand2,char operation);
-
 int main()
 {
-    srand((unsigned)time(nullptr));
-    int numProcesses;
-    cout << "How many processes do you want to create?: ";
-    cin >> numProcesses;
-    cin.get();
-    registerProcesses(numProcesses);
-    os.run();
-    cout << "Press ENTER to list all process info...." << endl;
-    cin.get();
-    os.printAllInfo();
+    System os;
+    os.startProgram();
     cout << "Press ENTER to finish...." << endl;
     cin.get();
-
     return 0;
 }
-
-void registerProcesses(int numProcesses){
-    char operations[] = {'+', '-', '*', '/', '%', '^'};
-    char operation;
-    int cont = 0,estimatedTime,operand1,operand2;
-    int id = 0;
-
-    while(cont < numProcesses){
-        id++;
-        operation = operations[rand() % 6];
-        operand1 = randInRange(-100, 100);
-        operand2 = randInRange(-100, 100);
-        operand2 = validateSecondOperand(operand2,operation);
-        estimatedTime = randInRange(6, 20);
-
-        createProcess(operation,operand1,operand2,estimatedTime,id);
-        cont++;
-    }
-}
-
-int randInRange(int lowBound, int highBound) {
-    return lowBound + (rand() % (highBound - lowBound + 1));
-}
-
-void createProcess(char operation,int operand1,int operand2,int estimatedTime,int id){
-
-    Process process(operation,operand1,operand2,estimatedTime,id);
-    os.addNewProcess(process);
-
-}
-
-int validateSecondOperand(int operand2,char operation){
-    if ((operation == '/' || operation == '%') && operand2 == 0){
-        operand2 = randInRange(-100, 100);
-    }
-    return operand2;
-}
-
-
 
